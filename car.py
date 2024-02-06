74bc1cacc963ca7362676aafd21dfff005fd7646
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-06 01:17:03 -0500

    extend vehicle class

--- car.py
+++ car.py
@@ -1,11 +1,11 @@
-class Car:
+class Vehicle:
     def __init__(self, started=False, speed=0) -> None:
         self.started = started
         self.speed = speed
 
     def start(self):
         self.started = True
-        print("Car started, let's ride!")
+        print("Started, let's ride!")
 
     def increase_speed(self, delta: int):
         if self.started:
@@ -17,6 +17,25 @@ class Car:
     def stop(self):
         self.speed = 0
         print('Halting')
+
+
+class Car(Vehicle):
+    trunk_open = False
+
+    def open_trunk(self):
+        self.trunk_open = True
+
+    def close_trunk(self):
+        self.trunk_open = False
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, center_stand_out=False) -> None:
+        self.center_stand_out = center_stand_out
+        super().__init__()
+
+    def start(self):
+        print('Sorry, out of fuel!')
 
 
 car = Car()
