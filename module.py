d0240dc686a00be1a892a01a39d531d4435f8f4d
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-06 16:26:25 -0500

    importing and modules

---
+++ module.py
@@ -0,0 +1,10 @@
+def my_function():
+    print('hello world')
+
+
+def greeter():
+    print('hello from module.py')
+
+
+if __name__ == '__main__':
+    greeter()
--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 import sys
+from module import my_function, greeter
 
 if len(sys.argv) == 2:
     name = sys.argv[1]
@@ -6,3 +7,20 @@ else:
     name = 'stranger'
 
 print(f'Hi there, {name}')
+
+
+def test():
+    a = 'a'
+
+    def me():
+        nonlocal a
+        a += 'b'
+
+    me()
+
+    print(a)
+
+
+test()
+
+my_function()
