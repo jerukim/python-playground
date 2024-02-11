a2bbc272b8fbc9bac548dcab5d2c6a42a36518f0
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-10 21:49:20 -0500

    add documentation to code with triple quotes and use help func

---
+++ docstring.py
@@ -0,0 +1,15 @@
+class MyDocumentedClass:
+    """This class is well documented but doesn't do anything special"""
+
+    def do_nothing(self):
+        """This method doesn't do anything but feel free to call it"""
+        pass
+
+
+# help(MyDocumentedClass)
+
+# help(print)
+
+help(list)
+
+# help(str)
