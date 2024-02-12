c92cce75cc2a9b13340c3fac9a5502d60b83bc82
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-12 15:19:11 -0500

    writing to file

---
+++ os.py
@@ -0,0 +1,41 @@
+# unsafe approach
+f = open('myfile.txt')
+print(f.read())
+f.close()
+
+
+# safe approach
+# opened file will only be available in code block
+with open('myfile.txt') as f:
+    text = f.read()
+
+print(text)
+
+
+# open file in "write" mode
+def printFile(fileName: str):
+    with open(fileName, 'r') as f:
+        print(f.read())
+
+
+with open('myfile.txt', 'w') as f:
+    for i in range(1, 5):
+        f.write(str(i))
+
+printFile('myfile.txt')
+
+
+# write new line
+with open('myfile.txt', 'w') as f:
+    for i in range(1, 5):
+        f.write(f'Number {i}\n')
+
+printFile('myfile.txt')
+
+
+# append to file
+with open('myfile.txt', 'a') as f:
+    for i in range(5, 8):
+        f.write(f'Append number {i}\n')
+
+printFile('myfile.txt')
